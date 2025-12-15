d03d54270376bb6af3dc43dc881206710aad2e5e
ppo3330 <junseok3330@gmail.com>
2025-12-15 13:53:08 +0900

    day6: 리뷰 수정 및 삭제 기능 추가

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('book/<int:pk>/', views.book_detail, name='book_detail'),
+    path('review/<int:pk>/edit/', views.review_edit, name='review_edit'),
+    path('review/<int:pk>/delete/', views.review_delete, name='review_delete'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Book, Review
 from .forms import ReviewForm
+from django.http import HttpResponseForbidden
 
 def home(request):
     books = Book.objects.all().order_by('-id')
@@ -27,3 +28,37 @@ def book_detail(request, pk):
         'form': form,
     }
     return render(request, 'reviews/detail.html', context)
+
+def review_edit(request, pk):
+    review = get_object_or_404(Review, pk=pk)
+
+    if request.user != review.user:
+        return HttpResponseForbidden("수정 권한이 없습니다.")
+    
+    if request.method == "POST":
+        form = ReviewForm(request.POST, instance=review)
+        if form.is_valid():
+            form.save()
+            return redirect('book_detail', pk=review.book.pk)
+    else:
+        form = ReviewForm(instance=review)
+
+    return render(request, 'reviews/review_edit.html', {
+    'form': form,
+    'review': review,
+})
+
+def review_delete(request, pk):
+    review = get_object_or_404(Review, pk=pk)
+
+    if request.user != review.user:
+        return HttpResponseForbidden("삭제 권한이 없습니다.")
+
+    if request.method == "POST":
+        book_pk = review.book.pk
+        review.delete()
+        return redirect('book_detail', pk=book_pk)
+        
+    return render(request, 'reviews/review_delete.html', {
+        'review': review,
+})
