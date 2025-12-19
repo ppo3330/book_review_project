273ca5cd954309b22ff00978caf21f43addc3cce
ppo3330 <junseok3330@gmail.com>
2025-12-19 20:28:32 +0900

    fix: superuser

--- accounts/apps.py
+++ accounts/apps.py
@@ -4,11 +4,3 @@ from django.apps import AppConfig
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
-    def ready(self):
-        from django.contrib.auth.models import User
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser(
-                username='admin',
-                email='admin@test.com',
-                password='admin1234'
-            )
--- bookreview_project/settings.py
+++ bookreview_project/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'reviews',
     'accounts',
-    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
